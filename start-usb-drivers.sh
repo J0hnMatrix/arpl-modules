552a9b4c2a61f058f5992f35e0fa7766f044585c
J0hnMatrix <bocal83@gmail.com>
2022-10-08 11:01:26 +0200

    Update start-usb-drivers.sh

--- start-usb-drivers.sh
+++ start-usb-drivers.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 case $1 in
   start)
-    insmod /lib/modules/usbserial.ko > /dev/null 2>&1
     insmod /lib/modules/cp210x.ko > /dev/null 2>&1
     insmod /lib/modules/ch341.ko > /dev/null 2>&1
     ;;
